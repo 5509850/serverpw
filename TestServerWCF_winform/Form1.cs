939636f369c8fb1e9df2018e29644260574da7d5
5509850 <5509850@gmail.com>
2015-11-23 23:07:20 +0300

    create new service WCF

---
+++ TestServerWCF_winform/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using TestServerWCF_winform.ServiceReference1;
+using TestServerWCF_winform.ServiceReference2;
+
+namespace TestServerWCF_winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Service1Client client = new Service1Client();
+
+            int vol = 0;
+            Int32.TryParse(textBox3.Text, out vol);
+            textBox1.Text = client.GetData(vol);
+            client.Close();
+        }
+    }
+}
