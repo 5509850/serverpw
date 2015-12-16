c3037b0aba015c7c9bea19e0e7a6851ca7d16d9d
5509850 <5509850@gmail.com>
2015-12-16 22:19:02 +0300

    add class utils - files

---
+++ TestServerWCF_winform/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServerWCF_winform
+{
+    class Utils
+    {
+        //https://lostinthegc.wordpress.com/2011/03/22/using-bcrypt-in-a-net-application-why-its-better-than-sha-or-md5/
+        //http://professorweb.ru/my/csharp/base_net/level2/2_3.php
+        public static string GetHashString(string s)
+        {
+            //переводим строку в байт-массим  
+            byte[] bytes = Encoding.Unicode.GetBytes(s);
+
+            //создаем объект для получения средст шифрования  
+            MD5CryptoServiceProvider CSP =
+                new MD5CryptoServiceProvider();
+
+            //вычисляем хеш-представление в байтах  
+            byte[] byteHash = CSP.ComputeHash(bytes);
+
+            string hash = string.Empty;
+
+            //формируем одну цельную строку из массива  
+            foreach (byte b in byteHash)
+                hash += string.Format("{0:x2}", b);
+
+            return hash;
+        }
+    }
+}
